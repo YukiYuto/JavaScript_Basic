c49d4941eb37c4a0ebc534e0f5358de702e76224
PhamQuan2003 <bestzed12345ff@gmail.com>
2024-06-23 11:18:00 +0700

    lession_3

---
+++ Lession_3/Lession_3.js
@@ -0,0 +1,27 @@
+let c = 5;
+let d = 5.9;
+
+//const là biến cố định không thể thay đổi giá trị
+const x = 5;
+
+
+//dấu backtick để bọc chuỗi string thì có thể dùng tất cả các dấu còn lại
+console.log(`hello "Quân" `, x)
+
+
+// function run() {
+//     var foo = "Foo";
+//     let bar = "Bar";
+//     console.log(foo, bar); // Foo Bar
+
+//     {
+//         var moo = "Mooo";
+//         let baz = "Bazz";
+//         console.log(moo, baz); // Mooo Bazz
+//     }
+
+//     console.log(moo); // Mooo
+//     console.log(baz); // ReferenceError
+// }
+
+// run();
